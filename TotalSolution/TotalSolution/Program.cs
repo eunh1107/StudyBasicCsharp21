4fbae1f89489e15539210835f722bcb04089314b
Eunheui Kim <dmsgml110712@gmail.com>
2021-05-03 11:12:31 +0900

    추가내용

--- TotalSolution/TotalSolution/Program.cs
+++ TotalSolution/TotalSolution/Program.cs
@@ -7,6 +7,7 @@ namespace TotalSolution
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!"); //주석
+            Console.WriteLine("Second print"); //두번째 주석
         }
     }
 }
