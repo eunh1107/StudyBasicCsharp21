6656b0b99c71a031f636deb655af09585f7c0aa0
Eunheui Kim <dmsgml110712@gmail.com>
2021-05-12 10:26:39 +0900

    동물

---
+++ TotalSolution/InheritTest/Dog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritTest
+{
+    class Dog : Animal //Animal을 상속받는다.
+    {
+        //특성 -> 속성
+        public string Color { get; set; }
+        
+        //기본생성자
+        public Dog() { this.Age = 0; this.Color = "흰색"; }
+
+        //액션 -> 메서드
+        public void Bark() 
+        {
+            Console.WriteLine  ("왕왕!"); 
+        }
+    }
+}
